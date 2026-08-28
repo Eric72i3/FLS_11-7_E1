34316e02d9a910d6763bfedcd8125e57cf252a92
Eric Möller <eric_moeller@icloud.com>
2026-08-28 12:47:32 +0000

    Hinzufügen eine programmes für einen steckbreif

---
+++ Steckbrief/Steckbrief.java
@@ -0,0 +1,25 @@
+package Steckbrief;
+import java.util.Scanner;
+
+public class Steckbrief {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int alter = 0;
+        char geschlecht = 'M';
+        String name = "";
+
+        System.out.println("Geben Sie den Wert für alter ein");
+        alter = sc.nextInt();
+        System.out.println("Geben Sie den Wert für geschlecht ein");
+        geschlecht = sc.next().charAt(0);
+        System.out.println("Geben Sie den Wert für name ein");
+        name = sc.next();
+
+        System.out.println("Steckbrief:");
+        System.out.println("Alter: " + alter);
+        System.out.println("Geschlecht: " + geschlecht);
+        System.out.println("Name: " + name);
+    }
+
+    
+}
