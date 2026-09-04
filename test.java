5473a37f63b5b5dc90f4f1b0c371e7af4a967e09
Eric Möller <eric_moeller@icloud.com>
2026-09-04 12:40:49 +0000

    test

--- test.java
+++ test.java
@@ -1,5 +1,12 @@
-void main() {
-    IO.println("Hallo aus dem Mini-Code!");
-    IO.println("So einfach ist die Konsolenausgabe jetzt.");
-}
+import static java.lang.IO.*;
 
+void main() {
+    // Einfache Ausgabe auf der Konsole
+    println("Moin! Wie heißt du?");
+    
+    // Benutzereingabe einlesen
+    String name = readln();
+    
+    // Formatierte Ausgabe mit dem eingegebenen Namen
+    println("Hi " + name + ", willkommen in Java 25!");
+}
